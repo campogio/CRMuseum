e3b6e1cbbaec0bc8befa20800d8fe8cdf4eb2953
Giordano Campogiani <campogio@hotmail.it>
2023-06-08 01:19:00 +0200

    Implementing Speech Recognition PT1

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {SpeechRecognition} from "@capacitor-community/speech-recognition";
+import any = jasmine.any;
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPage implements OnInit {
 
-  constructor() { }
+  recording = false;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
+    SpeechRecognition.requestPermissions()
+  }
+
+  async startRecognition(){
+      const {available} = await SpeechRecognition.available();
+
+      if (available){
+        this.recording= true;
+        SpeechRecognition.start({
+          popup: false,
+          partialResults: true,
+          language: 'en-US',
+        });
+
+        SpeechRecognition.addListener('partialResults',(data: any) =>{
+          console.log("Partial Result", data.matches)
+          //Works with Android, IOS needs data.matches
+          if(data.value && data.value.length > 0){
+            //do something with data.value[0]
+            this.changeDetectorRef.detectChanges();
+          }
+        });
+      }
+  }
+
+  async stopRecognition(){
+    this.recording=false;
+    await SpeechRecognition.stop();
+  }
 
   ngOnInit() {
   }
+
+
 
 }
