fb166ade3c279555c7b904497c0afc23f385e505
Giordano Campogiani <campogio@hotmail.it>
2023-06-08 19:43:09 +0200

    Working QR Scanner
    Augh

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -1,49 +1,57 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {SpeechRecognition} from "@capacitor-community/speech-recognition";
-import any = jasmine.any;
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
 })
+
+
+
 export class SearchPage implements OnInit {
 
-  recording = false;
 
   constructor(private changeDetectorRef: ChangeDetectorRef) {
     SpeechRecognition.requestPermissions()
-  }
 
-  async startRecognition(){
-      const {available} = await SpeechRecognition.available();
-
-      if (available){
-        this.recording= true;
-        SpeechRecognition.start({
-          popup: false,
-          partialResults: true,
-          language: 'en-US',
-        });
-
-        SpeechRecognition.addListener('partialResults',(data: any) =>{
-          console.log("Partial Result", data.matches)
-          //Works with Android, IOS needs data.matches
-          if(data.value && data.value.length > 0){
-            //do something with data.value[0]
-            this.changeDetectorRef.detectChanges();
-          }
-        });
-      }
-  }
-
-  async stopRecognition(){
-    this.recording=false;
-    await SpeechRecognition.stop();
   }
 
   ngOnInit() {
   }
+
+  prepare = () => {
+    BarcodeScanner.prepare();
+  };
+
+  async startScan(){
+    BarcodeScanner.hideBackground();
+    document.querySelector('body')!.classList.add('scanner-active');
+    const result = await BarcodeScanner.startScan();
+    if (result.hasContent) {
+      console.log(result.content);
+      this.stopScan();
+    }
+  };
+
+  stopScan = () => {
+    BarcodeScanner.showBackground();
+    BarcodeScanner.stopScan();
+    document.querySelector('body')!.classList.remove('scanner-active');
+  };
+
+  askUser(){
+    this.prepare();
+
+    const c = confirm('Do you want to scan a barcode?');
+
+    if (c) {
+      this.startScan();
+    } else {
+      this.stopScan();
+    }
+  };
 
 
 
