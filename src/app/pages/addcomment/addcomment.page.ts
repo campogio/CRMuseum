1a3f449c72ee5a1461711e04606e8214fc031c12
Giordano Campogiani <campogio@hotmail.it>
2023-06-13 17:13:38 +0200

    Guestbook entry page (camera functionality + text)
    needs implementation (saving the picture, saving the entry to DB)

--- src/app/pages/addcomment/addcomment.page.ts
+++ src/app/pages/addcomment/addcomment.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PhotoService} from "../../services/photo.service";
 
 @Component({
   selector: 'app-addcomment',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddcommentPage implements OnInit {
 
-  constructor() { }
+  constructor(public photoService: PhotoService) { }
 
   ngOnInit() {
   }
 
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
